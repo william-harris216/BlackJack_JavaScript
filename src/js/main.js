0e01d7505aec24c1e9f6811b977ed4120badd737
William <Williamharris2021@gmail.com>
2019-10-28 11:42:00 -0400

    Added DOM variables for user and to start game logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,49 @@
+import BlackJack from 'blackjack-dealer-logic'
+
+
+
+
+
 export default () => {
-    alert("Weclome to BlackJack")
-  }
+    
+    // // DOM variables
+    const game = BlackJack.singleDeckGame,
+          result = BlackJack.Result,
+          statsDisplay = document.getElementById('stats-display'),
+          newGameButton = document.getElementById('new-game-button'),
+          doubbleButton = document.getElementById('doubble-button'),
+          hitButton = document.getElementById('hit-button'),
+          stayButton = document.getElementById('stay-button'),
+          user = document.getElementById('user'),
+          userName = document.getElementById('user-name'),
+          userChipCount = document.getElementById('user-chip-count'),
+          userWager = document.getElementById('user-wager'),
+          dealer = document.getElementById("dealer"),
+          bust = document.getElementById('bust'),
+          outcome = document.getElementById('outcome'),
+          history = document.getElementById('history'),
+          reset = document.getElementById()
+          
+          ;
+
+    var gameHistory = [];
+    var userNameInput = window.prompt("What's your name?")
+    
+        
+
+    doubbleButton.style.display = 'none';
+    hitButton.style.display = 'none';
+    stayButton.style.display = 'none';
+
+    
+    newGameButton.addEventListener('click', function() {
+        gameOver = false;
+        playerWon = false;
+        
+        
+        newGameButton.style.display = 'none';
+        doubbleButton.style.display = 'inline';
+        hitButton.style.display = 'inline';
+        stayButton.style.display = 'inline';
+      });
+}
